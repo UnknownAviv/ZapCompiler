b983563b2bf081145f3232fa005aa37d39121bc2
daniel <danielserebro21@gmail.com>
2025-02-01 13:39:44 +0200

    half of the parsser done also all the lexer warks perfect

--- basic.py
+++ basic.py
@@ -1,9 +1,16 @@
+
+
+# IMPORTANT
+# If its the interwiwer from Hermon - as i said the project on its starting phase and only parts of the lexer are built in (also I didnt mention that my friend started parts of the parser).
+# The other parts of the compiler are not done yet so we are not commiting them in, i pushed and commited all the staff that was working fine.
+# If you need me for some questions about the project i can answer in any time. 
+
+1
 #######################################
 # CONSTANTS
 #######################################
 
 DIGITS = '0123456789'
-
 
 #######################################
 # ERRORS
@@ -15,16 +22,17 @@ class Error:
         self.pos_end = pos_end
         self.error_name = error_name
         self.details = details
+    
 
     def as_string(self):
         result = f'{self.error_name}: {self.details}\n'
         result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
         return result
 
-
 class IllegalCharError(Error):
     def __init__(self, pos_start, pos_end, details):
         super().__init__(pos_start, pos_end, 'Illegal Character', details)
+
 
 
 #######################################
@@ -72,6 +80,7 @@ class Token:
         self.type = type_
         self.value = value
 
+
     def __repr__(self):
         if self.value: return f'{self.type}:{self.value}'
         return f'{self.type}'
@@ -88,7 +97,7 @@ class Lexer:
         self.pos = Position(-1, 0, -1, fn, text)
         self.current_char = None
         self.advance()
-#oz shvarz
+
     def advance(self):
         self.pos.advance(self.current_char)
         self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
@@ -144,14 +153,85 @@ class Lexer:
             return Token(TT_INT, int(num_str))
         else:
             return Token(TT_FLOAT, float(num_str))
+        
+#######################################
+# NODES
+#######################################
 
+class NumberNode:
+	def __init__(self, tok):
+		self.tok = tok
+
+	def __repr__(self):
+		return f'{self.tok}'
+
+class BinOpNode:
+	def __init__(self, left_node, op_tok, right_node):
+		self.left_node = left_node
+		self.op_tok = op_tok
+		self.right_node = right_node
+
+	def __repr__(self):
+		return f'({self.left_node}, {self.op_tok}, {self.right_node})'
+    
+
+#######################################
+# PARSER
+#######################################
+
+class Parser:
+    def __init__(self,tokens):
+       self.tokens = tokens
+       self.tok_idx = -1
+       self.advance()
+
+    def advance(self,):
+        self.tok_idx += 1
+        if self.tok_idx < len(self.tokens):
+            self.current_tok = self.tokens[self.tok_idx]
+        return self.current_tok
+
+    def parse(self):
+        res = self.expr()
+        return res
+    
+
+    def factor(self):
+        tok = self.current_tok
+
+        if tok.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(tok)
+
+    def term(self):
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+
+    def expr(self):
+        return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
+
+    def bin_op(self, func, ops):
+        left = func()
+
+        while self.current_tok.type in ops:
+            op_tok = self.current_tok
+            self.advance()
+            right = func()
+            left = BinOpNode(left, op_tok, right)
+            
+        return left
 
 #######################################
 # RUN
 #######################################
 
 def run(fn, text):
+    # Generate tokens
     lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
+    if error: return None, error
 
-    return tokens, error
+    # Generate AST
+    parser = Parser(tokens)
+    ast = parser.parse()
+
+    return ast, None
--- shell.py
+++ shell.py
@@ -2,6 +2,7 @@ import basic
 
 while True:
     text = input('basic > ')
-    result, error = basic.run('<stdin>',text)
-    if error: print(error.as_str())
-    else: print (result)
+    result, error = basic.run('<stdin>', text)
+
+    if error: print(error.as_string())
+    else: print(result)
